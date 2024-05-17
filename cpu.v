32e2660cb9d8a517aaba44d0aeaa23e5b870582d
DebashishMukherjee <49284175+DebashishMukherjee@users.noreply.github.com>
2024-05-17 13:57:44 +0530

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -5,6 +5,17 @@ module cpu(input instruction,
 
     reg [31:0] pc;
 
-    reg [
+  reg [7:0] to_wtite;
+  wire [7:0] to_read1;
+  wire [7:0] to_read2;
+
+
+  always @(rst)
+    begin
+      if(rst == 1'b1)
+        pc = -4;
+    end
+
+  
 
 endmodule
